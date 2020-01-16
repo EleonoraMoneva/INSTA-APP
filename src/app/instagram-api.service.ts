2a00ad10619a405bf4ab5b9227c18bd904f97f27
Eleonora Moneva <58509263+EleonoraMoneva@users.noreply.github.com>
2020-01-16 19:00:42 +0100

    Add files via upload

--- src/app/instagram-api.service.ts
+++ src/app/instagram-api.service.ts
@@ -10,7 +10,9 @@ export class InstagramApiService {
   constructor(private http: HttpClient) { }
 
   getPosts(pageNumber: number):Observable<Post[]>{
-    return this.http.get<Post[]>(`https://5df115fd9df6fb00142bd818.mockapi.io/api/posts?page=${pageNumber}&limit=10`)
+    return this.http.get<Post[]>(`https://5e20938de31c6e0014c608c7.mockapi.io/api/v1/Users?page=${pageNumber}&limit=5`)
+    
+   // (`https://5df115fd9df6fb00142bd818.mockapi.io/api/posts?page=${pageNumber}&limit=10`)
   }
 
   getPostComments(postId: number):Observable<PostComment[]>{
@@ -18,7 +20,6 @@ export class InstagramApiService {
   }
 
   postComment(postId: number, comment: PostComment){
-    return this.http
-    .post(`http://5df115fd9df6fb00142bd818.mockapi.io/api/posts/${postId}/postComments`, comment);
+    return this.http.post(`http://5df115fd9df6fb00142bd818.mockapi.io/api/posts/${postId}/postComments`, comment);
   }
 }
