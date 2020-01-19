cbcac96a79c507d858166365a7df72595ed14e4f
Petar Ilievski <40742136+ilievski123p@users.noreply.github.com>
2020-01-19 03:07:03 +0100

    Update instagram-api.service.ts

--- src/app/instagram-api.service.ts
+++ src/app/instagram-api.service.ts
@@ -10,7 +10,7 @@ export class InstagramApiService {
   constructor(private http: HttpClient) { }
 
   getPosts(pageNumber: number):Observable<Post[]>{
-    return this.http.get<Post[]>(`https://5e20938de31c6e0014c608c7.mockapi.io/api/v1/Users?page=${pageNumber}&limit=5`)
+    return this.http.get<Post[]>(`https://5e20938de31c6e0014c608c7.mockapi.io/api/v1/Users?page=${pageNumber}&limit=1`)
     
    // (`https://5df115fd9df6fb00142bd818.mockapi.io/api/posts?page=${pageNumber}&limit=10`)
   }
