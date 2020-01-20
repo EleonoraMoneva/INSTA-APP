65018eebbfb378a64480ac8af8677295951d09d9
Petar Ilievski <40742136+ilievski123p@users.noreply.github.com>
2020-01-20 02:57:13 +0100

    Add files via upload

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { InstagramApiService } from '../instagram-api.service'
+import { AppComponent } from '../app.component'
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -7,9 +8,19 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 @Input() pictureUrl: string; 
-  constructor() { }
-
-  ngOnInit() {
+        desks= [];
+        voda:number=1;
+        desc=true;
+  constructor(private apiService: InstagramApiService)
+  {
   }
+  ngOnInit() {  
+    
+    this.apiService.getDescription(this.voda).subscribe((data : any[])=>
+    {console.log(data);
+     this.desks=data;
+    })
 
+  
+}
 }
