f234af82d265dde264fbfbce48fd8507d9d2949e
Petar Ilievski <40742136+ilievski123p@users.noreply.github.com>
2020-01-21 23:36:03 +0100

    Update instagram-api.service.ts

--- src/app/instagram-api.service.ts
+++ src/app/instagram-api.service.ts
@@ -10,11 +10,16 @@ export class InstagramApiService {
   constructor(private http: HttpClient) { }
 
   getPosts(pageNumber: number):Observable<Post[]>{
-    return this.http.get<Post[]>(`https://5e20938de31c6e0014c608c7.mockapi.io/api/v1/users?page=${pageNumber}&limit=5`)
-    
-   // (`https://5e20938de31c6e0014c608c7.mockapi.io/api/v1/users?page=${pageNumber}&limit=10`)
+    return this.http.get<Post[]>(`https://5e20938de31c6e0014c608c7.mockapi.io/api/v1/users?page=${pageNumber}&limit=1`)
   }
-
+  getDescription(pageNumber: number)
+  {
+    return this.http.get(`http://5e20938de31c6e0014c608c7.mockapi.io/api/v1/users/${pageNumber}/descriptions`)
+  }
+  getUsername(pageNumber: number)
+  {
+    return this.http.get(`http://5e20938de31c6e0014c608c7.mockapi.io/api/v1/users/${pageNumber}/name`)
+  }
   getPostComments(postId: number):Observable<PostComment[]>{
     return this.http.get<PostComment[]>(`http://5e20938de31c6e0014c608c7.mockapi.io/api/v1/users/${postId}/postComments`)
   }
