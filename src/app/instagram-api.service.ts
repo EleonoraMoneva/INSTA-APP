f594e080f4bbda026e0c24e0e66049c3f76a7f5d
RedHot17 <58517075+RedHot17@users.noreply.github.com>
2020-01-21 18:40:05 +0100

    Add files via upload

--- src/app/instagram-api.service.ts
+++ src/app/instagram-api.service.ts
@@ -10,24 +10,16 @@ export class InstagramApiService {
   constructor(private http: HttpClient) { }
 
   getPosts(pageNumber: number):Observable<Post[]>{
-    return this.http.get<Post[]>(`http://5e20938de31c6e0014c608c7.mockapi.io/api/v1/users?page=${pageNumber}&limit=1`)
-
+    return this.http.get<Post[]>(`https://5e20938de31c6e0014c608c7.mockapi.io/api/v1/users?page=${pageNumber}&limit=5`)
     
-   // (`https://5df115fd9df6fb00142bd818.mockapi.io/api/posts?page=${pageNumber}&limit=10`)
+   // (`https://5e20938de31c6e0014c608c7.mockapi.io/api/v1/users?page=${pageNumber}&limit=10`)
   }
-  getDescription(pageNumber: number)
-  {
-    return this.http.get(`http://5e20938de31c6e0014c608c7.mockapi.io/api/v1/users/${pageNumber}/descriptions`)
-  }
-  getUsername(pageNumber: number)
-  {
-    return this.http.get(`http://5e20938de31c6e0014c608c7.mockapi.io/api/v1/users/${pageNumber}/name`)
-  }
+
   getPostComments(postId: number):Observable<PostComment[]>{
     return this.http.get<PostComment[]>(`http://5e20938de31c6e0014c608c7.mockapi.io/api/v1/users/${postId}/postComments`)
   }
 
   postComment(postId: number, comment: PostComment){
-    return this.http.post(`http://5df115fd9df6fb00142bd818.mockapi.io/api/posts/${postId}/postComments`, comment);
+    return this.http.post(`http://5e20938de31c6e0014c608c7.mockapi.io/api/v1/users/${postId}/comments`, comment);
   }
 }
